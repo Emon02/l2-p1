b5df3653ee5582c3872fd660303f3ef076a32ab2
emon02 <abdulalimemon11@gmail.com>
2023-12-02 01:00:30 +0600

    create student interface, schema and model

---
+++ src/app/modules/students/student.model.ts
@@ -0,0 +1,89 @@
+import { Schema, model} from 'mongoose';
+import {
+  Guardian,
+  LocalGuardian,
+  Student,
+  UserName,
+} from './student.interface';
+
+// Create a Schema corresponding to the document interface.
+
+const userNameSchema = new Schema<UserName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+const gaurdianSchema = new Schema<Guardian>({
+  fatherName: {
+    type: String,
+    required: true,
+  },
+  fatherOccupation: {
+    type: String,
+    required: true,
+  },
+  fatherContactNo: {
+    type: String,
+    required: true,
+  },
+  motherName: {
+    type: String,
+    required: true,
+  },
+  motherOccupation: {
+    type: String,
+    required: true,
+  },
+  motherContactNo: {
+    type: String,
+    required: true,
+  },
+});
+
+const localGaurdianSchema = new Schema<LocalGuardian>({
+  name: {
+    type: String,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+  contactNo: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+const studentSchema = new Schema<Student>({
+  id: { type: String },
+  name: userNameSchema,
+  gender: ['male', 'female'],
+  email: { type: String, required: true },
+  dateOfBirth: { type: String },
+  contactNo: { type: String, required: true },
+  emergencyContactNo: { type: String, required: true },
+  bloodGroup: ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-'],
+  presentAddress: { type: String, required: true },
+  guardian: gaurdianSchema,
+  localGuardian: localGaurdianSchema,
+  profileImg: { type: String },
+  isActive: ['active', 'blocked'],
+});
+
+// Create a Model.
+
+const Student = model<Student>('Student', studentSchema);
